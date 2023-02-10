99f214be908d7f6817888aba1e1ac0c3e0c3815c
ArttuLe <lehtonen.arttu@yahoo.fi>
2023-02-10 13:01:35 +0200

    feat: WebApp allows model training

--- frontend.Dockerfile
+++ frontend.Dockerfile
@@ -1,12 +1,11 @@
-FROM python:3.9.0-buster
+FROM python:3.9.0-slim-buster
 
-
+RUN apt-get update && apt-get install libportaudio2 libusb-1.0 python3-opencv -y
 WORKDIR app
+ADD data /data/
 COPY requirements.txt .
 RUN pip3 install -r requirements.txt
 COPY . .
 
 
 CMD ["streamlit", "run", "TinyMLaaS.py"]
-
-
--- pages/4_Training.py
+++ pages/4_Training.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from io import BytesIO
+import matplotlib.pyplot as plt
+
+from tflm_hello_world.training import model_training
 
 st.set_page_config(
     page_title = 'Training',
@@ -6,8 +10,31 @@ st.set_page_config(
     layout = 'wide'
 )
 
+class model_training_vis():
+    def __init__(self):
+        self.plot = st.empty()
+        self.data = st.empty()
+        self.test = st.empty()
+
+    def render(self, history, epochs_range, model):
+        data = trainer.plot_statistics(history, epochs_range)
+        tests, label = trainer.prediction(model)
+        fig = trainer.plot_size()
+        
+        self.plot.image(data)
+        self.data.write(fig)
+        self.test.image(tests, caption=label)
+
+trainer = model_training()
+visual = model_training_vis()
 
 st.title('Training')
-st.header('Storing data')
-st.subheader('Labeling data')
-st.markdown('Training a model')
+
+st.subheader('Training a model')
+download = st.button('Train the model')
+
+if download:
+    status = st.text('Training the model...')
+    model, history,epochs_range = trainer.train_model(img_height=180, img_width=180, batch_size=32)
+    status.text('Model trained!')
+    visual.render(history, epochs_range, model)
