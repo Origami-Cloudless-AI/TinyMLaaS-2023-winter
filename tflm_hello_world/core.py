663078cd9c4a08c4b4774e301e5d05d33988c231
lifeofborna <borna.jamali@gmail.com>
2023-02-14 09:49:38 +0200

    fix: fixed nbdev_prepare not working, CI should pass

--- tflm_hello_world/core.py
+++ tflm_hello_world/core.py
@@ -3,10 +3,12 @@
 # %% auto 0
 __all__ = ['foo', 'say_hello']
 
-# %% ../nbs/00_core.ipynb 5
+# %% ../nbs/00_core.ipynb 3
 def foo(): pass
 
-# %% ../nbs/00_core.ipynb 6
+# %% ../nbs/00_core.ipynb 4
+from fastcore.test import *
+
 def say_hello(to):
     "Say hello to somebody"
     return f'Hello {to}!'
