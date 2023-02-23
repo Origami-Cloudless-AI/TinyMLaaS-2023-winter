720f1bac34ddc93aad45c66e4be7a75c36fc1ccf
lifeofborna <borna.jamali@gmail.com>
2023-02-23 14:22:55 +0200

    feat:Choose model from .json file. Visualize it on UI

--- pages/3_Model.py
+++ pages/3_Model.py
@@ -1,15 +1,48 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
-    page_title = 'Model',
-    page_icon = '✅',
-    layout = 'wide'
-)
+        page_title = 'Model',
+        page_icon = '✅',
+        layout = 'wide'
+    )
 
-st.title("Model")
 
-st.multiselect(
-    "Which models are needed? (max 3.)",
-    ["Object detection", "Face detection", "Vibration detection", "anomally detection"],
-    max_selections=3,
-)
+def save_selections():
+    st.info("Your selections have been saved",icon="✅")
+
+def create_default_page():
+    st.title("Model")
+    
+    models_df = read_file_with_models()
+    print(models_df)
+    #Display the models? 
+    col1, col2, col3= st.columns(3)
+    category = col1.radio('Select a category: ', models_df['Models'].keys())
+
+    if category:
+        models = models_df['Models'][category]
+        select_model = col2.radio("Select a model:",list(models.keys()))
+        
+        if select_model:
+            st.markdown(f" You have selected: **{select_model}** submodel under **{category}** model")
+            
+            table = "| Field | Description |\n| --- | --- | \n"
+            for key,item in models[select_model].items():
+                
+                table += f"| {key} | {item} \n"
+            
+            
+            st.markdown(table)
+    
+    col3.button("Select", on_click=save_selections)
+
+
+  
+def read_file_with_models():
+    "Reads csv file that has the models and sets it to pandas dataframe "
+    models_df = pd.read_json('pages/models.json')
+    return models_df
+
+
+create_default_page()
