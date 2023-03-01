55b6f909624af21ad7788fcaf8dbce829db5662d
lifeofborna <borna.jamali@gmail.com>
2023-03-01 17:46:48 +0200

    refactor:ui

--- TinyMLaaS.py
+++ TinyMLaaS.py
@@ -3,7 +3,6 @@ import socket
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
 
 
 st.set_page_config(
@@ -12,13 +11,10 @@ st.set_page_config(
     layout = 'wide'
 )
 
-st.title('TinyML as-a-Service')
-sock_conn = st.text('Socket status unknown')
-st.image(Image.open('tdd.png'), caption='3 device environments')
-
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
 
 def read_data_socket(sock_conn):
     
@@ -37,7 +33,6 @@ def read_data_socket(sock_conn):
                # if not data: break
                # conn.sendall(data)
 
-@st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
@@ -45,32 +40,44 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-read_data_socket(sock_conn)
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Done! (using st.cache)')
+def tinymlaas_page():
+    st.title('TinyML as-a-Service')
+    sock_conn = st.text('Socket status unknown')
 
-with st.expander("Click to read more"):
-    st.subheader('Raw data')
-    st.write(data)
+    alerts = [
+        {
+        "device_name": "Temp",
+        "alert_type": "low battery",
+        "alert_mes": "Battery is at 10%"
+        }
+    ]
+    read_data_socket(sock_conn)
+    # Create a text element and let the reader know the data is loading.
+    data_load_state = st.text('Loading data...')
+    # Load 10,000 rows of data into the dataframe.
+    data = load_data(10000)
+    # Notify the reader that the data was successfully loaded.
+    data_load_state.text('Done! (using st.cache)')
 
-    st.subheader('Number of pickups by hour')
-    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))
-    st.bar_chart(hist_values)
+    with st.expander("Click to read more"):
+        st.subheader('Raw data')
+        st.write(data)
 
-st.subheader('Map of all pickups')
-st.map(data)
+        st.subheader('Number of pickups by hour')
+        hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))
+        st.bar_chart(hist_values)
 
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+    st.subheader('Map of all pickups')
+    st.map(data)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+    hour_to_filter = st.slider('hour', 0, 23, 17)
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+    st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+    st.map(filtered_data)
 
+    if st.checkbox('Show raw data'):
+        st.subheader('Raw data')
+        st.write(data)
+
+tinymlaas_page()
 
--- pages/1_Device.py
+++ pages/1_Device.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+# Define the list of steps and their statuses
 
 st.set_page_config(
     page_title = 'Device',
@@ -10,18 +11,52 @@ st.set_page_config(
 
 st.title('Device')
 st.header('Register a device')
-st.markdown('https://streamlit-example-app-bug-report-streamlit-app-lrm3fx.streamlit.app/')
+
+new_device_name = st.text_input("Device name")
+new_device_connection = st.text_input("Connection")
+new_device_installer = st.text_input("Installer")
+new_compiler = st.text_input("Compiler")
+new_model = st.text_input("Model")
+new_description = st.text_input("Description")
+
+add_button = st.button("Add",key="add_button")
+delete_button = st.button("Remove",key="delete_button")
+
+df = pd.read_csv('TinyMLaaS.csv')
+
+def warning(selected_devices):
+    st.warning(selected_devices)
+
+if delete_button:
+    selected_devices = st.multiselect("Devices", options=df['Device name'].tolist(), key="selected_devices")
+
+    df = df[~df["Device name"].isin(selected_devices)]
+    df.to_csv("TinyMLaaS.csv", mode="w", index=False)
+
+
+if add_button: 
+    new_device_data = pd.DataFrame(
+        {
+        "Device name": [new_device_name],
+        "Connection": [new_device_connection],
+        "Compiler": [new_compiler],
+        "Model": [new_model],
+        "Description": [new_description]
+        }
+    )
+
+    df = pd.DataFrame(new_device_data)
+
+    df.to_csv('TinyMLaaS.csv',mode="a",index=False,header=False)
+    st.info("Added new data")
+
+
+df = pd.read_csv('TinyMLaaS.csv')
+
 st.header('List all registered devices')
 st.subheader('Filter devices with interactive table')
-st.markdown('https://streamlit-example-app-interactive-table-streamlit-app-mt9qg6.streamlit.app/')
+st.dataframe(df)
+
 st.subheader('Device location')
 st.markdown('https://streamlit-demo-uber-nyc-pickups-streamlit-app-456wus.streamlit.app/')
 
-st.text_input("device name")
-st.text_input("IP address")
-c1, c2, _ = st.columns((1, 2, 4))
-c1.button("Add", type="primary")
-c2.button("Remove")
-
-df = pd.read_csv('TinyMLaaS.csv')
-st.dataframe(df)
