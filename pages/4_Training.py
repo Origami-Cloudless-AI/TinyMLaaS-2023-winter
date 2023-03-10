ca9392efe2d1f08f2deb756ddbf0b1444ace5940
ArttuLe <lehtonen.arttu@yahoo.fi>
2023-03-10 12:09:43 +0200

    feat:UI redesign in training page

--- pages/4_Training.py
+++ pages/4_Training.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from tflm_hello_world.training import model_training
+from tflm_hello_world.training import train_model
 
 st.set_page_config(
     page_title='Training',
@@ -8,39 +8,35 @@ st.set_page_config(
 )
 
 
-class model_training_vis():
-    def __init__(self):
-        self.plot = st.empty()
-        self.data = st.empty()
-        self.test = st.empty()
-
-    def render(self, history, epochs_range, model, trainer):
-        data = trainer.plot_statistics(history, epochs_range)
-        tests, label = trainer.prediction(model)
-        fig = trainer.plot_size()
-
-        self.plot.image(data)
-        self.data.write(fig)
-        self.test.image(tests, caption=label)
-
-
-def training(trainer, visual):
-    status = st.text('Training the model...')
-    model, history, epochs_range = trainer.train_model(
-        img_height=180, img_width=180, batch_size=32)
-    status.text('Model trained!')
-    visual.render(history, epochs_range, model, trainer)
-    trainer.convert_to_c_array()
-
-
 def training_page():
-    trainer = model_training()
-    visual = model_training_vis()
-
+    train = train_model()
     st.title('Training')
+    st.subheader('Train a Keras model')
 
-    st.subheader('Training a model')
-    st.button('Train the model', on_click=training, args=(trainer, visual))
+    st.subheader("Model Training Settings")
+    epochs = st.number_input("Enter the number of epochs", min_value=int(0))
+    if epochs:
+        batch_size = st.number_input("Enter the batch size", min_value=int(0))
+        if batch_size:
+            img_width = st.number_input("Enter image width", min_value=int(0))
+            if img_width:
+                img_height = st.number_input("Enter image height", min_value=int(0))
+                if img_height:
+                    train_ds, test_ds = train.load_data(img_height, img_width, batch_size)
+                    if train_ds and test_ds:
+                        optim_choice = st.radio("Choose a loss function",("Some other :D", "Sparse Categorical crossentropy"))
+                        if optim_choice:
+                            if st.button("Train"):
+                                with st.spinner("Training..."):
+                                    plot = st.empty()
+                                    test = st.empty() 
+                                    model, history, epochs_range = train.train(img_height, img_width, epochs, optim_choice, train_ds, test_ds)
+                                st.success("Model trained successfully!")
 
-
+                                data = train.plot_statistics(history, epochs_range)
+                                tests, label = train.prediction(model, train_ds.class_names)
+                                plot.image(data)
+                                test.image(tests, caption=label)
+                                model.save(f"models/keras_model.h5")
+                                st.success("Model saved!")
 training_page()
