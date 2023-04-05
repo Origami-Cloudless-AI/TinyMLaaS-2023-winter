4e1b46e9883548b12445308d55904e4a88f2f145
Nella <nella.nieminen@gmail.com>
2023-04-05 14:05:49 +0300

    fix: RF tests for Data page

--- pages/2_Data.py
+++ pages/2_Data.py
@@ -52,20 +52,81 @@ def store_images(img_set, i, unlabeled=False, pil_img=False):
 
     return i
 
-def store_dataset(img_set, i):
-    " Stores a dataset from csv file to AWS S3"    
+def store_dataset(img_set):
+    " Store a dataset to AWS S3"
     for img in img_set:
-        st.image(img)
-        i += 1
+        s3_conn.upload_img(img, img.label, img.filename, True)
+    st.write("Images stored successfully!")
 
-    store_btn = st.button("Store images", key=f"button_{i}")
-    if store_btn:
-        for img in img_set:
-            s3_conn.upload_img(img, img.label, img.filename, True)
-        st.write("Images stored successfully!")
+def move_to_next_page(count_pages, current_page):
+    if current_page < count_pages - 1:
+        current_page += 1
+        st.session_state["current_page"] = current_page
+        st.experimental_rerun()
 
-    return i
+def move_to_previous_page(current_page):
+    if current_page > 0:
+        current_page -= 1
+        st.session_state["current_page"] = current_page
+        st.experimental_rerun()
 
+def display_current_page(images, current_page):
+    " Selects images for displaying"
+
+    num_cols, images_per_page = 5, 50
+
+    num_rows = len(images) // num_cols + (len(images) % num_cols > 0)
+
+    with st.container():
+        start_idx = current_page * images_per_page
+        end_idx = min(start_idx + images_per_page, len(images))
+        page_images = images[start_idx:end_idx]
+
+        for j in range(num_rows):
+            cols = st.columns(num_cols)
+            for k in range(num_cols):
+                index = j * num_cols + k
+                if index < len(page_images):
+                    cols[k].image(page_images[index], caption=f"{page_images[index].filename}")
+
+def display_images(images):
+    " Show max 50 images on each page in Data tab"
+
+    images_per_page = 50
+
+    count_pages = len(images) // images_per_page + (len(images) % images_per_page > 0)
+    current_page = st.session_state.get("current_page", 0)
+
+    button_container = st.container()
+
+    prev_col, page_col, next_col = button_container.columns([1, 1, 1])
+
+    if current_page > 0:
+        if prev_col.button("Previous", key=f"Previous"):
+            move_to_previous_page(current_page)
+
+    with page_col:
+        if count_pages != 0:
+            st.write(f"{current_page + 1}/{count_pages}")
+
+    if current_page < count_pages - 1:
+        if next_col.button("Next", key=f"Next"):
+            move_to_next_page(count_pages, current_page)
+
+    display_current_page(images, current_page)
+
+def update_stored_images():
+    " Fetching images from AWS/Localstack for viewing or deleting"
+
+    folders = ["Unlabeled", "0", "1"]
+
+    selected_folder = st.selectbox('Choose a folder', folders)
+    imgs = s3_conn.read_images(selected_folder)
+    if len(imgs) > 0:
+        if st.button(f"Remove images from {selected_folder}", key=f"remove_button_{i}"):
+            s3_conn.remove_objects(selected_folder)
+    
+    display_images(imgs)
                 
 st.set_page_config(
     page_title='Data',
@@ -118,6 +179,11 @@ def load_img(url):
     img = Image.open(url)
     return img
 
+st.header("Available devices")
+with st.expander("Click to choose devices"):
+    st.write("Not working :DD")
+
+st.header("Available datasets")
 with st.expander("Click to choose datasets"):
         for j in range(len(dataset_names)):
             st.write(dataset_names[j],dataset_sizes[j],dataset_descriptions[j])
@@ -128,16 +194,15 @@ with st.expander("Click to choose datasets"):
                 st.session_state["photo"] = "done"
                 for folder in os.listdir(dataset_locations[j]):
                     folderpath = dataset_locations[j]+folder
-                    max_images = 1
                     for image in os.listdir(folderpath):
                         file = folderpath+"/"+image
                         img = load_img(file)
                         img.filename = image
                         img.label = folder
                         file_images.append(img)
-                        max_images += 1
-                        if max_images > 100: # without paging the number of images is limited to 100
-                            break
+                store_btn = st.button("Store images", key=f"store_dataset")
+                if store_btn:
+                    store_dataset(file_images)
 
 uploaded_photo = col2.file_uploader(
     "Upload a photo", accept_multiple_files=True, on_change=change_photo_state)
@@ -152,12 +217,6 @@ if st.session_state["photo"] == "done":
     col2.success("Photo uploaded successfully")
 
     col3.metric(label="Temperature", value="-25℃ ", delta="3℃ ")
-    if len(file_images)>0:
-        st.header("Uploaded dataset")
-        with st.expander("Click to see uploaded dataset"):
-            i = store_dataset(file_images, i)
-            i += 1
-
     if camera_photo:
         st.header("Uploaded camera images")
         with st.expander("Click to see camera photos"):
@@ -172,7 +231,7 @@ if st.session_state["photo"] == "done":
             i = store_images(uploaded_photo, i)
             i += 1
 
-st.header("Unlabeled images")
+st.header("Label unlabeled images")
 UNLABELED_DIR = "Unlabeled/"
 
 with st.expander("Label unlabeled images"):
@@ -184,3 +243,6 @@ with st.expander("Label unlabeled images"):
         unlabeled_imgs = s3_conn.read_images(UNLABELED_DIR)
         i = store_images(unlabeled_imgs, i, unlabeled=True, pil_img=True)
         i += 1
+
+st.header("Stored images")
+update_stored_images()
